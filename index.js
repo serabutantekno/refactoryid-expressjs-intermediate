933e871f6120610178eb38996e288560c9af1166
serabutantekno <madyaneka@protonmail.com>
2021-04-26 18:13:26 +0700

    remove db connect test code

--- index.js
+++ index.js
@@ -12,17 +12,3 @@ app.get("/todos/", TodoController.get)
 app.listen(port, () => {
     console.log(`Listening at localhost:${port}`)
 })
-
-
-/**
- * Testing MySQL database connection.
- */
-const { Sequelize } = require("sequelize")
-const db = require("./src/config/database")
-const sequelize = new Sequelize(db.development)
-try {
-    sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-} catch (error) {
-    console.error('Unable to connect to the database:', error);
-}
