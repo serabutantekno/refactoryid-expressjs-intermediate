0216519ce98baaa85defc589d8e4d2fcaaa7573d
serabutantekno <madyaneka@protonmail.com>
2021-04-26 13:43:39 +0700

    setup db; testing db connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
-{
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
+    "username": "dev",
+    "password": "012345678",
+    "database": "refactory_expressjs",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
