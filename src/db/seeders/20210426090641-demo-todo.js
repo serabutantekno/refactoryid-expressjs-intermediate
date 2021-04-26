62cc324bf25ccd64c5721f8667812bc33fb35cb0
serabutantekno <madyaneka@protonmail.com>
2021-04-26 16:29:19 +0700

    using library faker to generate dummy data

--- src/db/seeders/20210426090641-demo-todo.js
+++ src/db/seeders/20210426090641-demo-todo.js
@@ -1,20 +1,24 @@
 'use strict';
 
+const faker = require("faker")
+
+const todos = () => {
+  const data = []
+  for (let i = 0; i <= 50 ; i++) {
+    data.push({
+      title: faker.lorem.sentence(),
+      desc: faker.lorem.sentence(),
+      status: true,
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent()
+    })
+  }
+  return data
+}
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Todos', [{
-      title: 'coffee',
-      desc: 'make a cup of coffee',
-      status: true,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      title: 'coffee 2',
-      desc: 'make a cup of coffee 2',
-      status: true,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }]);
+    return queryInterface.bulkInsert('Todos', todos());
   },
 
   down: async (queryInterface, Sequelize) => {
